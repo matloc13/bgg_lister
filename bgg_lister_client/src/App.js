2071e00dbd468356e99a99a14abbf04e77bd6594
Matthew Lockdall <mlockdall26@gmail.com>
2019-08-07 21:53:56 -0500

    created componets and sass folder

--- bgg_lister_client/src/App.js
+++ bgg_lister_client/src/App.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './main.css'
 
 function App() {
   return (
     <div className="App">
 
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
